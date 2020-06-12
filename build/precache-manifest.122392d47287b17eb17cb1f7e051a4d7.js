31117fb0cd28ebd67e69479dd8e940bfb1499342
richardcrete <47246887+richardcrete@users.noreply.github.com>
2020-06-12 03:46:55 +0200

    import build

---
+++ build/precache-manifest.122392d47287b17eb17cb1f7e051a4d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42a1dfd3a4c2794fcede35ad059a0aff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5cb309a774b8e17bb8ce",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7aeeaf94890d64750553",
+    "url": "/static/js/2.81b5e400.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.81b5e400.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cb309a774b8e17bb8ce",
+    "url": "/static/js/main.9a506cd1.chunk.js"
+  },
+  {
+    "revision": "18fae759f9ca207ce780",
+    "url": "/static/js/runtime-main.ceb6ffc5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
